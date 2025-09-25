9ec10bf66ea1629815918b2b9e437e6688bc0184
Beryl Hoffman <hoffmanb@elms.edu>
2025-09-25 15:52:37 -0400

    Fix data structure

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const exchangeRates = {
     "USD": {
       "USD": 1,
+      "EUR": 0.93,
       "KRW": 1392.27,
+      "RUP": 16583.00,
       // Add more "to" currencies and rates as needed
     },
+    
     "EUR": {
       "USD": 1.07,
       "KRW": 1639.69,
+      "RUP": 17824.00,
       // Add more "to" currencies and rates as needed
     },
 
@@ -15,18 +19,17 @@ const exchangeRates = {
 
     "RUP": {
       "USD": 0.000060,
+      "EUR": 0.000056,
+      "KRW": 0.088,
+      "RUP": 1,
       // Add more "to" currencies and rates as needed
     },
-    "USD": {
-      "RUP": 16583.00,
-      // Add more "to" currencies and rates as needed
-    }
-
 
     "KRW": {
       "USD": 0.00072,
       "EUR": 0.00061,
-    
+      "RUP": 0.011,
+      "KRW": 1,
     }
 
   };
